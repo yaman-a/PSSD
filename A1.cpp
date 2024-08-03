3c9c867871002c4655a2f8cb371d9556b5cedce1
yaman-a <yaman.ashqar05@gmail.com>
2024-08-03 21:24:14 +0930

    made it not a class

---
+++ A1.cpp
@@ -0,0 +1,11 @@
+#include "chooseName.hpp"
+
+int main() {
+
+    A1 sc;
+
+    cout << sc.chooseName("Mohammad Reza", "Yasaman Sadat", "Baqer Ali", "Boy") << endl;
+    cout << sc.chooseName("Mohammad Reza", "Yasaman Sadat", "Baqer Ali", "Girl") << endl;
+
+    return 0;
+}
--- chooseName.hpp
+++ chooseName.hpp
@@ -2,65 +2,61 @@
 
 using namespace std;
 
-class A1 {
-    public:
-    string chooseName(string father, string mother, string child1, string gender) {
-        string father1st;
-        string father2nd;
-        string mother1st;
-        string mother2nd;
-        string child1first;
-        string child1second;
+string chooseName(string father, string mother, string child1, string gender) {
+    string father1st;
+    string father2nd;
+    string mother1st;
+    string mother2nd;
+    string child1first;
+    string child1second;
 
-        splitName(father, father1st, father2nd);
-        splitName(mother, mother1st, mother2nd);
-        splitName(child1, child1first, child1second);
+    splitName(father, father1st, father2nd);
+    splitName(mother, mother1st, mother2nd);
+    splitName(child1, child1first, child1second);
 
-        string child1gender = getGender(child1first);
+    string child1gender = getGender(child1first);
 
-        if (child1gender == gender) {
-            // rule 2 - same gender
-            if (gender == "Boy") {
-                return father1st + " " + child1second;
-            } else {
-                return mother1st + " " + child1second;
-            }
+    if (child1gender == gender) {
+        // rule 2 - same gender
+        if (gender == "Boy") {
+            return father1st + " " + child1second;
         } else {
-            // rule 1 - different gender
-            if (gender == "Boy") {
-                return father2nd + " " + father1st;
-            } else {
-                return mother2nd + " " + mother1st;
-            }
+            return mother1st + " " + child1second;
         }
-    }
-
-    public:
-    void splitName(const string &fullName, string &first, string &second) {
-        first = "";
-        second = "";
-        bool foundspace = false;
-
-        for (int i = 0; i < fullName.size(); i++) {
-            if (fullName[i] == ' ') {
-                foundspace = true;
-            } else {
-                if (foundspace) {
-                    second += fullName[i];
-                } else {
-                    first += fullName[i];
-                }
-            }
-        }
-    }
-
-    string getGender(const string &name) {
-        char letter = name[0];
-        unordered_set<char> vowels = {'A', 'E', 'I', 'O', 'U', 'Y'};
-        if (vowels.find(letter) != vowels.end()) {
-            return "Girl";
+    } else {
+        // rule 1 - different gender
+        if (gender == "Boy") {
+            return father2nd + " " + father1st;
         } else {
-            return "Boy";
+            return mother2nd + " " + mother1st;
         }
     }
-};
+}
+
+void splitName(const string &fullName, string &first, string &second) {
+    first = "";
+    second = "";
+    bool foundspace = false;
+
+    for (int i = 0; i < fullName.size(); i++) {
+        if (fullName[i] == ' ') {
+            foundspace = true;
+        } else {
+            if (foundspace) {
+                second += fullName[i];
+            } else {
+                first += fullName[i];
+            }
+        }
+    }
+}
+
+string getGender(const string &name) {
+    char letter = name[0];
+    unordered_set<char> vowels = {'A', 'E', 'I', 'O', 'U', 'Y'};
+    if (vowels.find(letter) != vowels.end()) {
+        return "Girl";
+    } else {
+        return "Boy";
+    }
+}
