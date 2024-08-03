3c5564e1d00eb541e28a188ea94f9122a5b50641
yaman-a <yaman.ashqar05@gmail.com>
2024-08-03 20:43:38 +0930

    forgot to commit

---
+++ week1/inchworm.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+class inchworm {
+    public:
+    int lunchtime(int branch, int rest, int leaf) {
+        int count = 0;
+
+        for (int i = 0; i <= branch; i+= rest) {
+            if (i % leaf == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
---
+++ week1/test.cpp
@@ -0,0 +1,9 @@
+#include "inchworm.hpp"
+
+int main() {
+    inchworm worm;
+
+    std::cout << worm.lunchtime(1000,7,3) << std::endl;
+
+    return 0;
+}
